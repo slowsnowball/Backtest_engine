659db25b1cbad1f1753c4eec302875ba727b1fd5
liushihao <liushihao@pku.edu.cn>
2018-07-20 17:34:09 +0800

    Added object-oriented features and some more available functions

--- Asharetest.py
+++ Asharetest.py
@@ -5,52 +5,98 @@ import math
 import ssdata
 import matplotlib.pyplot as plt
 
+
 ###############################################################################
-#                         Define the functions needed                         #
+#                           Define framework classes                          #
 ###############################################################################
 
+class account:
+    def __init__(self, start_date, end_date, capital_base, freq, benchmark,
+                 universe, tax=0.001, commission=0.00025, slippage=0.01):
+        """
+        start_date: the start date of back test
+        end_date: the end date of back test
+        capital_base: initial fund to perform back test
+        freq: back test frequencies, measured in days, eg. 1 for daily and 7
+            for weekly
+        tax: tax rate
+        commission: commission rate
+        slippage: slippage
+        """
+        self.start_date = start_date
+        self.end_date = end_date
+        self.capital_base = capital_base
+        self.freq = freq
+        self.benchmark = benchmark
+        self.universe = universe
+        self.tax = tax
+        self.commission = commission
+        self.slippage = slippage
+        self.ini_dic = {}
+        self.benchmark_data = pd.DataFrame()
+        self.trade_days = None
+        self.order_days = None
+        self.today_capital = None
+        self.ret = pd.DataFrame()
+        self.history_max = 0
+        self.drawdown_start = None
+        self.drawdown_end = None
+        self.capital = []
+        self.cash = capital_base
 
-def initialize():
-    # initialize stock information
 
-    global n, trade_days, order_days, ini_dic, ret, benchmark, history_max
-    history_max = 0
-    ini_dic = {}
-    for stock in universe:
+###############################################################################
+#                          Define framework functions                         #
+###############################################################################
+
+def account_setup(account):
+    """
+    Set up the ini_dic, trade_days and order_days of account.
+    """
+    for stock in account.universe:
         try:
-            ini_dic[stock] = ssdata.get_data(secid=stock,
-                                             start_date=start_date,
-                                             end_date=end_date,
-                                             field='open').sort_index()
+            data = ssdata.get_data(secid=stock,
+                                   start_date=account.start_date,
+                                   end_date=account.end_date,
+                                   field='open').sort_index()
+            account.ini_dic[stock] = data
         except Exception:
-            universe.remove(stock)
-            print(stock, " data unavailable.")
-    n = 0
-    trade_days = ini_dic[universe[0]].index
-    order_days = get_order_days(freq)
-    ret = pd.DataFrame()
-    benchmark = pd.read_csv('file:///Users/liushihao/emacs/python/\
-benchmarkdata.csv', index_col=0)
+            account.universe.remove(stock)
+            print("Stock ", stock, " data unavailable.")
+
+    try:
+        data = ssdata.get_data(secid=account.benchmark,
+                               start_date=account.start_date,
+                               end_date=account.end_date,
+                               field='open').sort_index()
+        account.benchmark_data = account.benchmark_data.append(data)
+    except Exception:
+        print("Benchmark ", account.benchmark, " data unavailable.")
+    account.trade_days = account.ini_dic[account.universe[0]].index
+    account.order_days = get_order_days(account)
 
 
-def get_order_days(freq):
-    tdays = list(trade_days)
+def get_order_days(account):
+    """
+    Get order days based on frequency.
+    """
+    tdays = list(account.trade_days)
     days = []
     for i in range(len(tdays)):
-        if i % freq == 0:
+        if i % account.freq == 0:
             days.append(tdays[i])
     return days
 
 
-def handle_data(date):
-    positions = pd.Series()
-    for stock in universe:
-        positions[stock] = 0.5
-    order_pct_to(positions)
+def order_to(target):
+    global h_amount
+    trade_days = account.trade_days
+    order_days = account.order_days
+    tax = account.tax
+    commission = account.commission
+    ini_dic = account.ini_dic
+    today_capital = account.today_capital
 
-
-def order_pct_to(target):
-    global drawdown_start, drawdown_end, history_max, cash, n, h_amount, ret
     if date in order_days:
         print(date.strftime('%Y-%m-%d'), list(target.index))
         t_amount = pd.DataFrame({'tamount': [0]}, index=list(target.index))
@@ -60,8 +106,10 @@ def order_pct_to(target):
             if stock not in list(target.index):
                 stock_data = ini_dic[stock].loc[date.strftime('%Y-%m-%d')]
                 price = stock_data['open']
-                cash += h_amount.loc[stock, 'hamount']*(price-0.01)*(1-tax-commission)
-                print('order: ', stock, 'amount ', int(0-h_amount.loc[stock, 'hamount']))
+                account.cash += h_amount.loc[stock, 'hamount'] *\
+                    (price-0.01) * (1-tax-commission)
+                print('order: ', stock, 'amount ',
+                      int(0-h_amount.loc[stock, 'hamount']))
                 h_amount.drop(stock)
 
         # Deal with stocks in target
@@ -73,90 +121,127 @@ def order_pct_to(target):
             if stock not in list(h_amount.index):
                 h_amount = h_amount.append(pd.DataFrame({'hamount': [0],
                                                          'price': [0],
-                                                         'value': [0]}, index=[stock]))
+                                                         'value': [0],
+                                                         'percent': [0]},
+                                                        index=[stock]))
 
-            t_amount.loc[stock, 'tamount'] = math.floor((target[stock] * today_capital) / (100 * price)) * 100
+            t_amount.loc[stock, 'tamount'] = math.floor(target[stock]/100)*100
 
             # If hoding > target, sell
-            if h_amount.loc[stock, 'hamount'] - t_amount.loc[stock, 'tamount'] > 0:
-                cash += (h_amount.loc[stock, 'hamount'] - t_amount.loc[stock, 'tamount'])*(price-0.01)*(1-tax-commission)
+            if h_amount.loc[stock, 'hamount'] - t_amount.loc[stock, 'tamount']\
+               > 0:
+                account.cash += (h_amount.loc[stock, 'hamount'] -
+                                 t_amount.loc[stock, 'tamount'])\
+                                 * (price-0.01) * (1-tax-commission)
 
             # If hoding < target, buy
-            if h_amount.loc[stock, 'hamount'] - t_amount.loc[stock, 'tamount'] < 0:
+            if h_amount.loc[stock, 'hamount'] - t_amount.loc[stock, 'tamount']\
+               < 0:
                 # Attention: buy hand by hand in case cash becomes negative
-                for number in range(int(t_amount.loc[stock, 'tamount']/100), 0, -1):
-                    if cash - (number*100-h_amount.loc[stock, 'hamount'])*(price+0.01)*(1+commission) < 0:
+                for number in range(int(t_amount.loc[stock, 'tamount']/100),
+                                    0, -1):
+                    if account.cash - (number*100 -
+                                       h_amount.loc[stock, 'hamount']) *\
+                       (price+0.01) * (1+commission) < 0:
                         continue
                     else:
-                        cash -= (number*100-h_amount.loc[stock, 'hamount'])*(price+0.01)*(1+commission)
+                        account.cash -= (number*100 -
+                                         h_amount.loc[stock, 'hamount']) *\
+                            (price+0.01) * (1+commission)
                         t_amount.loc[stock, 'tamount'] = number * 100
                         break
 
-            if h_amount.loc[stock, 'hamount'] - t_amount.loc[stock, 'tamount'] != 0:
-                print('order: ', stock, 'amount ', int(t_amount.loc[stock, 'tamount'] - h_amount.loc[stock, 'hamount']))
+            if h_amount.loc[stock, 'hamount'] - t_amount.loc[stock, 'tamount']\
+               != 0:
+                print('order: ', stock, 'amount ',
+                      int(t_amount.loc[stock, 'tamount'] -
+                          h_amount.loc[stock, 'hamount']))
 
             h_amount.loc[stock, 'hamount'] = t_amount.loc[stock, 'tamount']
             h_amount.loc[stock, 'price'] = price
-            h_amount.loc[stock, 'value'] = h_amount.loc[stock, 'price'] * h_amount.loc[stock, 'hamount']
+            h_amount.loc[stock, 'value'] = h_amount.loc[stock, 'price'] *\
+                h_amount.loc[stock, 'hamount']
 
         h_amount['percent'] = h_amount['value'] / sum(h_amount['value'])
 
     # # Output holding details
     # h_amount.to_csv('position_details.csv')
 
-    capital.append(today_capital)
+    account.capital.append(today_capital)
     try:
-        drawdown = (max(capital[:-1])-capital[-1])/(max(capital[:-1]))
+        drawdown = (max(account.capital[:-1])-account.capital[-1]) /\
+            max(account.capital[:-1])
     except Exception:
         drawdown = 0
 
-    if drawdown > history_max:
-        drawdown_start = trade_days[capital.index(max(capital[:-1]))]
-        drawdown_end = trade_days[capital.index(capital[-1])]
-        history_max = drawdown
+    if drawdown > account.history_max:
+        account.drawdown_start =\
+            trade_days[account.capital.index(max(account.capital[:-1]))]
+        account.drawdown_end =\
+            trade_days[account.capital.index(account.capital[-1])]
+        account.history_max = drawdown
 
-    ret = ret.append(pd.DataFrame(
-        {'rev': (capital[-1]-capital[0])/capital[0],
-         'max_drawdown': history_max,
-         'benchmark': (benchmark.loc[date.strftime('%Y/%-m/%-d'), 'openIndex']
-                       -
-                       benchmark.loc['2015/1/5', 'openIndex']) /
-         benchmark.loc['2015/1/5', 'openIndex']}, index=[date]))
+    account.ret = account.ret.append(pd.DataFrame(
+        {'rev': (account.capital[-1]-account.capital[0])/account.capital[0],
+         'max_drawdown': account.history_max,
+         'benchmark':
+         (account.benchmark_data.loc[date.strftime('%Y-%m-%d'), 'open'] -
+          account.benchmark_data.loc[trade_days[0].strftime('%Y-%m-%d'),
+                                     'open']) /
+         account.benchmark_data.loc[trade_days[0].strftime('%Y-%m-%d'), 'open']},
+        index=[date]))
 
-    n += 1
+
+def order_pct_to(pct_target):
+    ini_dic = account.ini_dic
+    today_capital = account.today_capital
+    target = pd.Series()
+
+    for stock in list(pct_target.index):
+        stock_data = ini_dic[stock].loc[date.strftime('%Y-%m-%d')]
+        price = stock_data['open']
+        target[stock] = (pct_target[stock]*today_capital) / price
+
+    order_to(target)
 
 
-def result_display():
-    ret.to_csv('return_details.csv')
+def result_display(account):
+    """
+    Display results, including the return curve and a table showing returns
+    drawdown and drawdown intervals.
+    """
+    # account.ret.to_csv('return_details.csv')
     # strategy annual return
-    Ra = ((1+(ret.iloc[-1].rev))**(250/n)) - 1
-    df = pd.DataFrame({'benchmark return':
-                       '%.2f%%' % (ret.iloc[-1].benchmark * 100),
-                       'Strategy return':
-                       '%.2f%%' % (ret.iloc[-1].rev * 100),
-                       'Strategy annual return':
-                       '%.2f%%' % (Ra*100),
-                       'Max drawdown':
-                       '%.2f%%' % (ret.iloc[-1].max_drawdown*100),
-                       'Max drawdown interval':
-                       str(drawdown_start.strftime('%Y-%m-%d')
-                           + ' to '
-                           + drawdown_end.strftime('%Y-%m-%d'))},
-                      index=[''])
-    df.reindex(['benchmark return',
-                'Strategy return',
-                'Strategy annual return',
-                'Max drawdown',
-                'Max drawdown interval'], axis=1)
-    print(df.transpose())
+    Ra = (1+(account.ret.iloc[-1].rev)) **\
+        (250/len(list(account.trade_days))) - 1
+    results = pd.DataFrame({'benchmark return':
+                            '%.2f%%' % (account.ret.iloc[-1].benchmark * 100),
+                            'Strategy return':
+                            '%.2f%%' % (account.ret.iloc[-1].rev * 100),
+                            'Strategy annual return':
+                            '%.2f%%' % (Ra*100),
+                            'Max drawdown':
+                            '%.2f%%' % (account.ret.iloc[-1].max_drawdown*100),
+                            'Max drawdown interval':
+                            str(account.drawdown_start.strftime('%Y-%m-%d')
+                                + ' to '
+                                + account.drawdown_end.strftime('%Y-%m-%d'))},
+                           index=[''])
+    results.reindex(['benchmark return',
+                     'Strategy return',
+                     'Strategy annual return',
+                     'Max drawdown',
+                     'Max drawdown interval'], axis=1)
+    print(results.transpose())
 
     # plot the results
-    ret['rev'].plot(color='royalblue', label='strategy return')
-    ret['benchmark'].plot(color='black', label='benchmark return')
-    x = np.array(list(ret.index))
-    plt.fill_between(x, max(max(ret.rev), max(ret.benchmark)),
-                     min(min(ret.rev), min(ret.benchmark)),
-                     where=((x <= drawdown_end) & (x >= drawdown_start)),
+    account.ret['rev'].plot(color='royalblue', label='strategy return')
+    account.ret['benchmark'].plot(color='black', label='benchmark return')
+    x = np.array(list(account.ret.index))
+    plt.fill_between(x, max(max(account.ret.rev), max(account.ret.benchmark)),
+                     min(min(account.ret.rev), min(account.ret.benchmark)),
+                     where=((x <= account.drawdown_end) &
+                            (x >= account.drawdown_start)),
                      facecolor='lightsteelblue',
                      alpha=0.4)
     plt.legend()
@@ -164,40 +249,59 @@ def result_display():
 
 
 ###############################################################################
-#                         Set the backtest parameters                         #
+#                   Parameters and functions set up manually                  #
 ###############################################################################
 
+print("Hello world!")
 start_date = '2015-01-01'
 end_date = '2016-01-01'
-capital = []
-position = 0.
-cash = 1000000
+capital_base = 1000000
 freq = 1
-tax = 0.001
-commission = 0.00025
+benchmark = ['000001.XSHE']
 universe = ['600036.XSHG', '601166.XSHG']
+
+
+def initialize(account):
+    """
+    This is a function that runs only once, before the backtest begins.
+    """
+    pass
+
+
+def handle_data(account):
+    """
+    This is a function that runs every backtest frequency.
+    """
+    positions = pd.Series()
+    for stock in account.universe:
+        positions[stock] = 0.5
+    order_pct_to(positions)
+
 
 ###############################################################################
 #                               Backtest begins                               #
 ###############################################################################
 
-initialize()
 
-for date in trade_days:
-    if n == 0:
-        today_capital = position + cash
-        h_amount = pd.DataFrame({'hamount': [0],
-                                 'price': [0],
-                                 'value': [0],
-                                 'percent': [0]}, index=universe)
-    else:
-        today_capital = 0
-        for stock in list(h_amount.index):
-            stock_data = ini_dic[stock].loc[date.strftime('%Y-%m-%d')]
-            price = stock_data['open']
-            today_capital += price * h_amount.loc[stock, 'hamount']
-        today_capital += cash
+account = account(start_date=start_date, end_date=end_date,
+                  capital_base=capital_base, freq=freq,
+                  benchmark=benchmark, universe=universe)
+account_setup(account)
+initialize(account)
 
-    handle_data(date)
+h_amount = pd.DataFrame({'hamount': [0],
+                         'price': [0],
+                         'value': [0],
+                         'percent': [0]}, index=universe)
 
-result_display()
+for date in account.trade_days:
+    account.today_capital = 0
+    for stock in list(h_amount.index):
+        stock_data = account.ini_dic[stock].loc[date.strftime('%Y-%m-%d')]
+        price = stock_data['open']
+        account.today_capital += price * h_amount.loc[stock, 'hamount']
+    account.today_capital += account.cash
+
+    handle_data(account)
+
+result_display(account)
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-Backtest Engine
+# Backtest Engine
+1. delete the "Parameters and functions set up manually" region.
+
+2. Import this module.
+
+3. Set your own parameters and functions.
+
+Basic parameters include start_date, end_date, capital_base, freq, benchmark, universe. Tax, commission, slippage will set to 0.001, 0.00025, 0.01 by default.
+
+Basic functions include initialize(account) and handle_data(account). In handle_data(account) you can design your own strategy and you can define other functions to support your strategy. Ordering functions you can apply in your strategy include order_pct_to(pct_target) and order_to(target).
+
+Function initialize(account) will be run only once when the backtest starts, and handle_data(account) will be run multiple times based on the frequency.
+
+More functions will be added to this module in the future. :)
+
+4. When all set up, just run your code. 
